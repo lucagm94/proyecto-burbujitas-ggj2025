ece1fcf8a1e321644f9e47c3b10a59dac9747289
Luca Moyano <luca-moyano@hotmail.com>
2025-01-21 19:45:59 -0300

    Se implemento el movimiento del personaje en 8 direcciones

--- nuevo-proyecto-de-juego/Scripts/MC/MC.gd
+++ nuevo-proyecto-de-juego/Scripts/MC/MC.gd
@@ -4,22 +4,21 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	mover_mc(delta)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func mover_mc(delta) -> void: 
+	var direccion = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		direccion.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direccion.y += 1
+	if Input.is_action_pressed("ui_left"):
+		direccion.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direccion.x += 1
+	
+	direccion = direccion.normalized()
+	velocity = direccion * SPEED
+	
 	move_and_slide()
