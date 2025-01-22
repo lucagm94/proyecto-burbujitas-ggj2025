7d61cbbad8a5cb7be799603f5aafc7eeb55c3f4a
Luca Moyano <luca-moyano@hotmail.com>
2025-01-21 21:59:23 -0300

    Se implemento sistema de disparo de burbuja

--- nuevo-proyecto-de-juego/Scripts/MC/MC.gd
+++ nuevo-proyecto-de-juego/Scripts/MC/MC.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+var bubble = preload("res://Scenes/MC/Shots/Bublee_shoot.tscn")
+var shots:Array = ["Bubble","Air"]
+var currentShot = "Bubble"
 
 func _physics_process(delta: float) -> void:
 	mover_mc(delta)
+
+	
+func _process(delta: float) -> void: 
+	seleccionar_disparo()
+	disparar()
+	
 
 func mover_mc(delta) -> void: 
 	var direccion = Vector2.ZERO
@@ -22,3 +30,26 @@ func mover_mc(delta) -> void:
 	velocity = direccion * SPEED
 	
 	move_and_slide()
+
+func seleccionar_disparo() -> void:
+	if Input.is_action_just_pressed("1"):
+		currentShot = shots[0]
+	if Input.is_action_just_pressed("2"):
+		currentShot = shots[1]
+		
+func disparar() -> void: 
+	if Input.is_action_just_pressed("left_click"):
+		var mousePosition = get_global_mouse_position()
+		var direccion = (mousePosition - global_position).normalized()
+		var origenBala = global_position + direccion * 80
+		#var bala;
+		if currentShot == shots[0]:
+			var bala = bubble.instantiate()
+			bala.global_position = origenBala
+			get_tree().root.add_child(bala)
+			bala.aplicar_fuerza_inicial(direccion)
+			print("deberia disparar")
+			pass
+		if currentShot == shots[1]:
+			pass
+		
---
+++ nuevo-proyecto-de-juego/Scripts/MC/Shots/bublee_shoot.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@export var speed = 600
+var velocidadInicial = 100
+var impulsoInicial = true
+
+func _physics_process(delta: float) -> void:
+	velocity.y = -speed * delta
+	move_and_slide()
+	
+func aplicar_fuerza_inicial(direccion: Vector2) -> void: 
+	if impulsoInicial: 
+		velocity = direccion * velocidadInicial
+
+
+func _on_timer_fuerza_timeout() -> void:
+	if impulsoInicial: 
+		if (velocity.x > 0):
+			velocity.x = velocity.x -4
+			$TimerFuerza.stop()
+			$TimerFuerza.start()
+		if (velocity.x < 0):
+			velocity.x += 4
+			$TimerFuerza.stop()
+			$TimerFuerza.start()
+		if velocity.x < 1 and velocity.x > -1:
+			$TimerFuerza.stop()
+			impulsoInicial = false
+	
+	
+	
