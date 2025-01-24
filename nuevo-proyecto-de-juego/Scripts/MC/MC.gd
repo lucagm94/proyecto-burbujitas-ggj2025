6e514ce150a6f5c0c229e063cca78071287f7fec
srrod92 <rodrigogonzalez1992@hotmail.com>
2025-01-24 14:10:04 -0300

    Update MC.gd
    SE modifica el mc para que los disparos se hagan con click primario y click secundario

--- nuevo-proyecto-de-juego/Scripts/MC/MC.gd
+++ nuevo-proyecto-de-juego/Scripts/MC/MC.gd
@@ -4,8 +4,6 @@ const SPEED = 300.0
 
 var bubble = preload("res://Scenes/MC/Shots/Bublee_shoot.tscn")
 var air = preload("res://Scenes/MC/Shots/Air.tscn")
-var shots:Array = ["Bubble","Air"]
-var currentShot = "Bubble"
 var bullet
 
 func _physics_process(delta: float) -> void:
@@ -14,7 +12,6 @@ func _physics_process(delta: float) -> void:
 
 	
 func _process(delta: float) -> void: 
-	seleccionar_disparo()
 	disparar()
 	
 
@@ -34,48 +31,35 @@ func mover_mc(delta) -> void:
 	
 	move_and_slide()
 
-func seleccionar_disparo() -> void:
-	if Input.is_action_just_pressed("1"):
-		currentShot = shots[0]
-		print("Disparo 1 seleccionado")
-	if Input.is_action_just_pressed("2"):
-		currentShot = shots[1]
-		print("Disparo 2 seleccionado")
-		
+
 func disparar() -> void: 
 	
-	if Input.is_action_just_pressed("left_click"):
+	if Input.is_action_just_pressed("left_click"): #
 		var mousePosition = get_global_mouse_position()
 		var direccion = (mousePosition - global_position).normalized()
 		var origenBala = global_position + direccion * 80
-		#var bala;
-		if currentShot == shots[0]:
-			var bala = bubble.instantiate()
-			bala.global_position = origenBala
-			get_tree().root.add_child(bala)
-			bala.aplicar_fuerza_inicial(direccion)
-			print("deberia disparar")
-			pass
-		if currentShot == shots[1]:
-			bullet = air.instantiate()
-			add_child(bullet)
-			bullet.global_position = global_position
-			bullet.rotation= direccion.angle()
-			pass
-			
 		
-	if Input.is_action_pressed("left_click"):
-		if currentShot == shots[1]:
-			var mousePosition = get_global_mouse_position()
-			var direccion = (mousePosition - global_position).normalized()
-			bullet.rotation = direccion.angle()
-			
-	if Input.is_action_just_released("left_click"):
-		print("Click soltado")
-		if currentShot == shots[1]:
-			bullet.queue_free()
-			bullet=null
-			#get_tree().root.find_child("Air",true,false).queue_free()
-			
+		var bala = bubble.instantiate()
+		bala.global_position = origenBala
+		get_tree().root.add_child(bala)
+		bala.aplicar_fuerza_inicial(direccion)
+		print("deberia disparar")
 
+	if Input.is_action_just_pressed("right_click"):
+		var mousePosition = get_global_mouse_position()
+		var direccion = (mousePosition - global_position).normalized()
+		bullet = air.instantiate()
+		add_child(bullet)
+		bullet.global_position = global_position
+		bullet.rotation= direccion.angle()
+		pass
+			
 		
+	if Input.is_action_pressed("right_click"):
+		var mousePosition = get_global_mouse_position()
+		var direccion = (mousePosition - global_position).normalized()
+		bullet.rotation = direccion.angle()
+			
+	if Input.is_action_just_released("right_click"):
+		print("Click soltado")
+		bullet.queue_free()
