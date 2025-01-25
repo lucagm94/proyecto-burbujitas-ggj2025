b4fc9917418f22b40072217752393cabe923076a
Luca Moyano <luca-moyano@hotmail.com>
2025-01-24 21:13:03 -0300

    Se implemento barra de progreso y timer

---
+++ nuevo-proyecto-de-juego/Scripts/SistemaOxigeno/ControladorOxigeno.gd
@@ -0,0 +1,42 @@
+extends Control
+var minutos = 5
+var segundos = 0
+var totalTime;
+var tiempoTranscurrido = 0 
+
+func _ready():
+	
+	totalTime = minutos*60 + segundos
+	$TextureProgressBar.max_value = totalTime
+	actualizar_tiempo();
+
+func quitar_oxigeno(valorADescontar: int)-> void:
+	segundos -= valorADescontar
+	tiempoTranscurrido += 1 
+	if segundos <= 0:
+		minutos -=1 
+		segundos = 59
+	actualizar_tiempo()
+
+func agregar_oxigeno(valorAAgregar)->void:
+	segundos += valorAAgregar
+	tiempoTranscurrido -= 1
+	if segundos >= 60:
+		segundos = 0 
+		minutos +=1 
+	actualizar_tiempo()
+	
+func _on_descontar_segundo_timeout() -> void:
+	quitar_oxigeno(1)
+
+func actualizar_tiempo():
+	if segundos < 10  && minutos <10:
+		$Texto.text = "0" + str(minutos) + ":0" + str(segundos)
+	elif segundos >10 && minutos < 10: 
+		$Texto.text = "0" + str(minutos) + ":" + str(segundos)
+	elif segundos < 10 && minutos > 10:
+		$Texto.text = str(minutos) + ":0" + str(segundos)
+	else: 
+		$Texto.text = str(minutos) + ":" + str(segundos)
+	
+	$TextureProgressBar.value = tiempoTranscurrido
