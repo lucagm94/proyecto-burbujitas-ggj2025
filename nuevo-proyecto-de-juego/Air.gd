468d786d8a39eb54df53d1b1ddf33b77fe568b1a
Milo Vargas <80860312+MiloVarok@users.noreply.github.com>
2025-01-25 17:06:35 -0300

    MuchasCosas

--- nuevo-proyecto-de-juego/Air.gd
+++ nuevo-proyecto-de-juego/Air.gd
@@ -2,7 +2,13 @@ extends Area2D
 #Para que el empuje sea efectivo se debe activar las colisiones en las leyer y mask 10 del objeto a mover
 var fuerzaDeEmpuje  = 1
 
+func _ready() -> void:
+	$Sound_secador.play()
+	
 func _physics_process(delta):
 	for body in get_overlapping_bodies():
 		var direccion:Vector2 = global_transform.x.normalized()
 		body.apply_impulse(direccion*fuerzaDeEmpuje)
+
+func _on_timer_timeout() -> void:
+	get_tree().call_group("ControlOxigeno", "quitar_oxigeno", 1)
--- nuevo-proyecto-de-juego/Scripts/MC/Shots/bublee_shoot.gd
+++ nuevo-proyecto-de-juego/Scripts/MC/Shots/bublee_shoot.gd
@@ -29,3 +29,14 @@ func _on_timer_fuerza_timeout() -> void:
 		if velocity.x < 1 and velocity.x > -1:
 			$TimerFuerza.stop()
 			impulsoInicial = false
+
+
+func _on_bubble_area_area_entered(area: Area2D) -> void:
+	if area.get_parent().is_in_group("RompeBurbuja"):
+		Sounds.play_sfx("BubleeExplosion")
+		queue_free()
+
+
+func _on_destruir_timeout() -> void:
+	queue_free()
+	pass # Replace with function body.
