6c2b33656819d6e09b4601c724622113fe8cb574
Luca Moyano <luca-moyano@hotmail.com>
2025-01-26 11:03:07 -0300

    Se implemento condicion de derrota

--- nuevo-proyecto-de-juego/Scripts/MC/MC.gd
+++ nuevo-proyecto-de-juego/Scripts/MC/MC.gd
@@ -5,13 +5,16 @@ const SPEED = 300.0
 var bubble = preload("res://Scenes/MC/Shots/Bublee_shoot.tscn")
 var air = preload("res://Scenes/MC/Shots/Air.tscn")
 var bullet
+
+var muerte = false
+
 signal consumir_oxigeno(oxigenoUtilizado: int)
 signal recuperar_oxigeno(oxigenoRecuperado: int)
 
 
 func _physics_process(delta: float) -> void:
-	
-	mover_mc(delta)
+	if !muerte:
+		mover_mc(delta)
 
 func _process(delta: float) -> void: 
 	disparar()
@@ -33,50 +36,52 @@ func mover_mc(delta) -> void:
 	move_and_slide()
 
 func disparar() -> void: 
-	
-	if Input.is_action_just_pressed("left_click"): 
-		var mousePosition = get_global_mouse_position()
-		var direccion = (mousePosition - global_position).normalized()
-		var origenBala = global_position + direccion * 80
-		
-		var bala = bubble.instantiate()
-		bala.global_position = origenBala
-		get_tree().root.add_child(bala)
-		bala.aplicar_fuerza_inicial(direccion)
-		
-		emit_signal("consumir_oxigeno",1)
-
-	if Input.is_action_just_pressed("right_click"):
-		var mousePosition = get_global_mouse_position()
-		var direccion = (mousePosition - global_position).normalized()
-		bullet = air.instantiate()
-		add_child(bullet)
-		bullet.global_position = global_position
-		bullet.rotation= direccion.angle()
-		emit_signal("consumir_oxigeno",1)
-		pass
+	if !muerte:
+		if Input.is_action_just_pressed("left_click"): 
+			var mousePosition = get_global_mouse_position()
+			var direccion = (mousePosition - global_position).normalized()
+			var origenBala = global_position + direccion * 80
 			
-		
-	if Input.is_action_pressed("right_click"):
-		var mousePosition = get_global_mouse_position()
-		var direccion = (mousePosition - global_position).normalized()
-		bullet.rotation = direccion.angle()
-		
+			var bala = bubble.instantiate()
+			bala.global_position = origenBala
+			get_tree().root.add_child(bala)
+			bala.aplicar_fuerza_inicial(direccion)
 			
-	if Input.is_action_just_released("right_click"):
-		print("Click soltado")
-		bullet.queue_free()
+			emit_signal("consumir_oxigeno",1)
 
-
-# func _on_area_2d_body_entered(body: Node2D) -> void:
-#	if body.name == "Bubble":
-#		emit_signal("recuperar_oxigeno", 10)
-#		body.queue_free()
-#	pass # Replace with function body.
-
+		if Input.is_action_just_pressed("right_click"):
+			var mousePosition = get_global_mouse_position()
+			var direccion = (mousePosition - global_position).normalized()
+			bullet = air.instantiate()
+			add_child(bullet)
+			bullet.global_position = global_position
+			bullet.rotation= direccion.angle()
+			emit_signal("consumir_oxigeno",1)
+			pass
+				
+			
+		if Input.is_action_pressed("right_click"):
+			var mousePosition = get_global_mouse_position()
+			var direccion = (mousePosition - global_position).normalized()
+			bullet.rotation = direccion.angle()
+			
+				
+		if Input.is_action_just_released("right_click"):
+			print("Click soltado")
+			bullet.queue_free()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == "BubbleArea":
 		emit_signal("recuperar_oxigeno", 1)
 		area.get_parent().queue_free()
 		Sounds.play_sfx("BubleeExplosion")
+
+func _on_sistema_de_oxigeno_derrota() -> void:
+	if bullet != null:
+		bullet.queue_free()
+	muerte = true
+	visible = false
+
+func reiniciar():
+	visible = true
+	muerte = false
--- nuevo-proyecto-de-juego/Scripts/State/Falling.gd
+++ nuevo-proyecto-de-juego/Scripts/State/Falling.gd
@@ -3,9 +3,7 @@ class_name Falling
 
 func _enter(machine_reference):
 	super._enter(machine_reference)
-	print("Ingresando al state Falling")
 	if machine is RigidBody2D:
-		print("aplicando fuerza")
 		machine.add_constant_central_force(Vector2(0,500))
 	pass
 	
@@ -15,6 +13,5 @@ func doingHisShit():
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(body) #remplaza al metodo de la señal
 	if body.is_in_group("Ground"): #no encontre mejor manera para identificar a la burbuja
-		print("Cambiando a idle")
 		machine._changeState(IdleObject.new())
 	pass
